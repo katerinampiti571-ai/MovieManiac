a9271fc4de02698033cf2c2efb03293a7c3e3c12
Katerina Biti <katerinabiti@192.168.2.11>
2026-08-16 13:16:34 +0300

    Rename app to VideoClub

--- app/src/main/java/com/example/videoclub/VideoClubApplication.kt
+++ app/src/main/java/com/example/videoclub/VideoClubApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MovieManiacApp: Application()
+class VideoClubApp: Application()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MovieManiac"
+rootProject.name = "VideoClub"
 include(":app")
